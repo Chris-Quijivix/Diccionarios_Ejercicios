f17c0b29d1a4b24c0905a2bb4987ec420782a4d8
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-06 04:19:30 -0600

    Ejercicio 3

---
+++ Diccionarios3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ejercicio 3, Diccionarios");
+
+Dictionary<string,double> nombre = new Dictionary<string,double>();
+
+nombre.Add("Juan", 56.2);
+nombre.Add("Pedro", 89.3);
+nombre.Add("Carlos", 20);
+nombre.Add("Maria", 95);
+
+Console.WriteLine("La nota del estudiante Carlos es:" + nombre["Carlos"]);
+
+Console.ReadKey();
+
+
