0e258181a560fb3565f41211c82a0b44edc06155
chris <chris@MSI>
2026-03-06 03:46:53 -0600

    Agregar archivos de proyecto.

---
+++ Diccionarios/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Cree un diccionario que almacene ID (int) y nombre (string) de 3 estudiantes. Luego muestre el nombre de uno específico usando su ID");
+Console.WriteLine();
+
+Dictionary<int,string> nombre = new Dictionary<int,string>();
+
+nombre.Add(1, "Juan");
+nombre.Add(2, "Pedro");
+nombre.Add(3, "Carlos");
+nombre.Add(4, "Maria");
+
+int buscar  = 1;
+Console.WriteLine("El estudiante con el ID (1) es:" + nombre[buscar]);
+
+Console.ReadKey();
