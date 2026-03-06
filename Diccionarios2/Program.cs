47e13b99829d59b94fd2582fd1ba6fd0bdde19b7
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-06 04:07:40 -0600

    .

--- Diccionarios2/Program.cs
+++ Diccionarios2/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Cree un diccionario donde la clave sea el código de producto (int) y el valor el nombre del producto. Agregue 3 productos e imprímalos");
 Console.WriteLine();
+Console.WriteLine();
 
 Dictionary<int,string> codigo = new Dictionary<int,string>();
 
