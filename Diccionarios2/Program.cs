48a3287541123684b78f3c9d94e8f8679f2ca7fe
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-06 04:07:06 -0600

    Agregue segundo ejercicio

--- Diccionarios2/Program.cs
+++ Diccionarios2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Cree un diccionario donde la clave sea el código de producto (int) y el valor el nombre del producto. Agregue 3 productos e imprímalos");
+Console.WriteLine();
+
+Dictionary<int,string> codigo = new Dictionary<int,string>();
+
+codigo.Add(123, "Arroz");
+codigo.Add(234, "Aceite");
+codigo.Add(345, "Arina");
+
+foreach (var item in codigo)
+{
+    Console.WriteLine($"El codigo del producto es:{item.Key} y el producto es:{item.Value}");
+}
+
+Console.ReadKey();
