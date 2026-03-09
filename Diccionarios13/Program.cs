a3f4d74739e4ae20c3c3b07cb632b867db341224
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 03:04:01 -0600

    Ejercicio 13 agregado

---
+++ Diccionarios13/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Ejercicio 13, Diccionarios");
+Console.WriteLine();
+
+Dictionary<string,double> nota = new Dictionary<string,double>();
+
+nota.Add("Juan", 85);
+nota.Add("Isaac", 52);
+nota.Add("Mateo", 75);
+nota.Add("Pedro", 92);
+nota.Add("Alejandro", 61);
+
+double suma  = 0;
+
+Console.WriteLine("Lista de estudiantes y notas:");
+
+foreach (var item in nota)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
+
+Console.WriteLine();
+
+foreach (var item in nota)
+{
+    suma += item.Value;
+}
+
+double promedio;
+promedio = suma / nota.Count;
+
+Console.WriteLine($"El promedio de las notas es de: {promedio}");
+
+Console.ReadKey();
