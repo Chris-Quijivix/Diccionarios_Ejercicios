56b9f920effe8b08fb5a9fcc6c42280e99111eab
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 03:16:59 -0600

    Ejercicio 15 agregado

---
+++ Diccionarios15/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Ejercicio 15, Diccionarios");
+Console.WriteLine();
+
+Dictionary<int,string> empleado = new Dictionary<int,string>();
+
+empleado.Add(1, "Juan");
+empleado.Add(2, "Mateo");
+empleado.Add(3, "Isaac");
+
+Console.WriteLine("Listado de empleados:");
+Console.WriteLine();
+
+foreach (var item in empleado)
+{
+    Console.WriteLine($"ID: {item.Key} - Nombre: {item.Value}");
+}
+
+Console.ReadKey();
