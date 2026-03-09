6ba66dfb4f373f218fc0feaae10ba14f4c0ed8c5
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 02:38:25 -0600

    Ejercicio 10 agregado

---
+++ Diccionarios10/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Ejercicio 10, Diccionarios");
+Console.WriteLine();
+
+Dictionary<int,string> inventario = new Dictionary<int,string>();
+
+inventario.Add(1, "Monitor");
+inventario.Add(2, "CPU");
+inventario.Add(3, "Teclado");
+
+Console.Write("Ingrese codigo de producto a eliminar: ");
+int codigo;
+codigo = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (inventario.ContainsKey(codigo))
+{
+    inventario.Remove(codigo);
+    Console.WriteLine("El producto se a eliminado");
+}
+else
+{
+    Console.WriteLine("El producto no esta registrado");
+}
+
+Console.ReadKey();
