a7189cea19b00b4c32741c0f272adb2fea5267d2
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 03:12:18 -0600

    Ejercicio 14 agregado

---
+++ Diccionarios14/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Ejercicio 14, Diccionarios");
+Console.WriteLine();
+
+Dictionary<int,string> ciudad = new Dictionary<int,string>();
+
+ciudad.Add(1, "Quetzaltenango");
+ciudad.Add(2, "Guatemala");
+ciudad.Add(3, "Antigua Guatemala");
+ciudad.Add(4, "Chimaltenango");
+ciudad.Add(5, "Escuintla");
+
+Console.WriteLine("Codigos de ciudades:");
+
+foreach (var item  in ciudad)
+{
+    Console.WriteLine($"Codigo de ciudad: {item.Key}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Ciudades:");
+
+foreach (var item in ciudad)
+{
+    Console.WriteLine($"Ciudad: {item.Value}");
+}
+
+Console.ReadKey();
