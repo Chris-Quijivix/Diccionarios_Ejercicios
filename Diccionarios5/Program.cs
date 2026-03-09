26e0d1a6f477890b373dc2f3ed02709a336a5b90
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 01:45:52 -0600

    Ejercicio 5 agregado

---
+++ Diccionarios5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Ejercicio 5, Diccionarios");
+Console.WriteLine();
+
+Dictionary<string,string> usuario = new Dictionary<string,string>();
+
+usuario.Add("Carlos", "car123");
+usuario.Add("Juan", "juan4235");
+usuario.Add("Maria", "mar5465");
+
+Console.WriteLine("La contraseña del usuario (Juan) es:" + usuario["Juan"]);
+
+Console.ReadKey();
