09a1c8486193e54c2b1b9b626f3c0c6e133369a3
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 02:15:52 -0600

    Ejercicio 8 agregado

---
+++ Diccionarios8/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Ejercicio 8, Diccionarios");
+Console.WriteLine();
+
+Dictionary<int,string> empleado = new Dictionary<int,string>();
+
+empleado.Add(1, "Carlos");
+empleado.Add(2, "Juan");
+empleado.Add(3, "Maria");
+empleado.Add(4, "Pedro");
+
+empleado[2] = "Mateo";
+
+Console.WriteLine("Lista de empleados actualizada:");
+Console.WriteLine();
+
+foreach (var item in empleado)
+{
+    Console.WriteLine($"Codigo: {item.Key}, Nombre nuevo: {item.Value}");
+}
+
+Console.ReadKey();
