303b6fac3541e3811947650399e532e9ba098d95
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 01:38:42 -0600

    Ejercicio 4 agregado

---
+++ Diccionarios4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Ejercicio 4, Diccionarios");
+Console.WriteLine();
+
+Dictionary<int,string> ciudad = new Dictionary<int,string>();
+
+ciudad.Add(1,"Quetzaltenango");
+ciudad.Add(2,"Guatemala");
+ciudad.Add(3,"Antigua Guatemala");
+ciudad.Add(4,"Chimaltenango");
+ciudad.Add(5,"Escuintla");
+
+Console.WriteLine("La ciudad con el codigo (1) es:" + ciudad[3]);
+
+Console.ReadKey();
