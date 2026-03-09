e064eba84e7e40d8d528a2fb3e2c56883b8264eb
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 01:51:38 -0600

    Ejercicio 6 agregado

---
+++ Diccionarios6/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Ejercicio 6, Diccionarios");
+Console.WriteLine();
+
+Dictionary<int,string> estudiantes = new Dictionary<int,string>();
+
+estudiantes.Add(1, "Carlos");
+estudiantes.Add(2, "Juan");
+estudiantes.Add(3, "Maria");
+
+estudiantes.Remove(2);
+
+foreach (var item in estudiantes)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
+
+Console.ReadKey();
